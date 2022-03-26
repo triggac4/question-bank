14e4aec25a66c9654b55cbf95b0697fb767cab6a
kene3055 <koffokansi@gmail.com>
2022-03-26 13:18:04 +0100

    centralize main content

--- src/components/main_content.jsx
+++ src/components/main_content.jsx
@@ -4,7 +4,7 @@ import ModalContext from "../provider";
 const MainContent = () => {
     const { setModal } = useContext(ModalContext);
     return (
-        <main className="my-16 flex flex-col justify-center items-center gap-8">
+        <main className="my-auto h-4/5 flex flex-col justify-center items-center gap-8">
             <h4 className="font-bold">
                 Welcome To El Academy's Question Application
             </h4>
