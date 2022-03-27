36ec0eadd8199f52dee37614df0b22b16c6104e5
kene3055 <koffokansi@gmail.com>
2022-03-27 12:05:11 +0100

    reading files check

--- backend/router/question.js
+++ backend/router/question.js
@@ -1,7 +1,11 @@
+const fs = require("fs");
 const router = require("express").Router();
 
+const resultboolean = fs.readFileSync("./question/day 2/boolean-Answer.docx");
+
+const result = fs.readdirSync("./question", "utf8");
 router.route("/").get(async (req, res) => {
-    res.json({ msg: "get request" });
+    res.send(result);
 });
 
 module.exports = router;
