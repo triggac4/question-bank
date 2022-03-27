492a52b5a9fcdb36f401a57e64a2e5fddbc929b2
kene3055 <koffokansi@gmail.com>
2022-03-27 14:57:35 +0100

    front end setup

--- frontend/src/components/select_questions.jsx
+++ frontend/src/components/select_questions.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState, useLayoutEffect } from "react";
+
+import API from "../util/api_consumption";
 import Button from "./button";
 import Select from "./select";
+
 const Form = () => {
+    const [result, setResult] = useState([]);
+    const [course, setCourse] = useState("");
+    useLayoutEffect(() => {
+        const apiConsumer = new API();
+        const set = async () => {
+            const response = await API.asyncWrapper(apiConsumer.getFileNames());
+            if (response) {
+                setResult(response.result);
+            }
+        };
+        set();
+    }, []);
+    let courses = [];
+    let sections = [];
+    result.forEach((e) => {
+        courses.push(e.course);
+        if (course === e.course) {
+            sections = e.section;
+        }
+    });
     return (
         <form className="flex flex-col justify-around h-full gap-4 items-center">
-            <Select
-                label="course"
-                name="course name"
-                options={["Python", "C.C.N.A", "Javascript", "PHP"]}
-            />
-            <Select
-                label="section"
-                name="section"
-                options={["section 1", "section 2", "section 3", "section 4"]}
-            />
+            <Select label="course" name="course name" options={courses} />
+            <Select label="section" name="section" options={sections} />
             <Button text="fetch Assignment" />
         </form>
     );
---
+++ frontend/src/util/api_consumption.js
@@ -0,0 +1,29 @@
+class API {
+    static serverhost = "http://localhost:4000/api/v1/question";
+    static async asyncWrapper(func) {
+        try {
+            return await func();
+        } catch (err) {
+            return false;
+        }
+    }
+    async getFileNames() {
+        const result = await fetch(`${API.serverhost}/courses`, {
+            method: "GET",
+        });
+        if (result.status > 199 && result.status < 300) {
+            return await result.json();
+        } else {
+            throw Error(" something went wrong!");
+        }
+    }
+
+    async getPath(course, filename) {
+        const courseParsed = course.replaceAll(" ", "%");
+        const filenameParsed = filename.replaceAll(" ", "%");
+        const path = `${API.serverhost}/course?course=${courseParsed}&filename=${filenameParsed}`;
+        return path;
+    }
+}
+
+export default API;
